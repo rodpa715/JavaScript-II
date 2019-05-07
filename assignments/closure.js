09ea73328d557611df3fc7449571913960c849d3
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-07 14:18:25 +0200

    finished assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const outside = "I am global, I am god, omnipresent at all times, but I can't see what's inside"
+console.log(outside)
+function house(){
+  const livingRoom = "I am locked inside the house but can see outside, but not the closet"
+  console.log(outside)
+  console.log(livingRoom)
+  function closet(){
+    const tshirt = "I locked insite the closet but can see into the living room and outside"
+    console.log(outside)
+    console.log(livingRoom)
+    console.log(tshirt)
+  }
+  closet();
+}
+house()
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
