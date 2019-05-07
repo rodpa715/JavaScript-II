a91b7af7bbdcf5702704b144acd6d1497f05511e
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-07 13:59:50 +0200

    finished assignments

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,8 +1,8 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
-/* 
+/*
 
   //Given this problem: 
   
@@ -14,7 +14,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
   // Higher order function using "cb" as the call back
   function firstItem(arr, cb) {
-    return cb(arr[0]);
+    cb(arr[0]);
   }
 
   // Function invocation 
@@ -24,28 +24,42 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
+function logger(arg){ //logger for console
+  console.log(arg)
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  const len = arr.length;
+  cb(`The length of the array is: ${len}`);
 }
+getLength(items, logger)
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(`The last item of the array is: ${arr[arr.length - 1]}`);
 }
+last(items, logger)
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x +y);
 }
+sumNums(3,3, logger)
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
+multiplyNums(2,10, logger)
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item))
 }
-
+contains("Gum", items, logger)
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
